547eb411ff1b20ed92b0a67029ec7db2a1fd5609
yeongji@bbang <bbangji1994@naver.com>
2023-09-17 00:00:06 +0900

    fea ) modelMapper

---
+++ src/main/java/com/analyze/analyst/core/ModelConfig.java
@@ -0,0 +1,13 @@
+package com.analyze.analyst.core;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
